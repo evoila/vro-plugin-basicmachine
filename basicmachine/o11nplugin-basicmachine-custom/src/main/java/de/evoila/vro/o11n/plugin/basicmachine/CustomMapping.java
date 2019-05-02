9abbdd787a8d88c6d22ff1352a3223243b71db6b
Lars Atzinger <latzinger@evoila.de>
2019-05-02 11:17:34 +0200

    added BasicMachineManager as singeltion

--- basicmachine/o11nplugin-basicmachine-custom/src/main/java/de/evoila/vro/o11n/plugin/basicmachine/CustomMapping.java
+++ basicmachine/o11nplugin-basicmachine-custom/src/main/java/de/evoila/vro/o11n/plugin/basicmachine/CustomMapping.java
@@ -3,12 +3,14 @@ package de.evoila.vro.o11n.plugin.basicmachine;
 import com.vmware.o11n.sdk.modeldrivengen.mapping.AbstractMapping;
 import de.evoila.vro.o11n.plugin.basicmachine.finder.BasicMachineFinder;
 import de.evoila.vro.o11n.plugin.basicmachine.model.BasicMachine;
+import de.evoila.vro.o11n.plugin.basicmachine.model.BasicMachineManager;
 import de.evoila.vro.o11n.plugin.basicmachine.relater.BasicMachineRelater;
 
 public class CustomMapping extends AbstractMapping {
 
     @Override
     public void define() {
+        singleton(BasicMachineManager.class);
         wrap(BasicMachine.class).andFind().using(BasicMachineFinder.class).withIcon("default-16x16.png");
         relateRoot().to(BasicMachine.class).using(BasicMachineRelater.class).as("basicmachines");
     }
